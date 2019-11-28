e3ef50fd8b0005685d1639a70b49f201a1104240
Nedzad Gicic <nedzad.gicic@hpe.com>
2019-11-28 14:29:43 +0100

    Test commit.

--- src/ArrayLinkiedLists/Grocery.java
+++ src/ArrayLinkiedLists/Grocery.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class Grocery {
     private ArrayList<String>groceryList = new ArrayList<String>();
 
-    public void addGroceryList(String item){
+    public void addGroceryList(String item) {
+        String nickname = "Necko";
         this.groceryList.add(item);
     }
-    public void printGroceryList(){
+    public void printGroceryList() {
         System.out.println("You have "+this.groceryList.size()+" items in grocery list");
+
         for (int i = 0; i < groceryList.size() ; i++) {
             System.out.println((i+1)+"."+groceryList.get(i));
         }
