5a87c8e3946d11cdfe4f6ddfd90d53f398d92734
Eldin Tiro <eldin.tiro@edu.fit.ba>
2019-12-04 01:44:51 +0100

    Sorted collections - Basket example

---
+++ src/Collections/Map/SortedCollection/Main.java
@@ -0,0 +1,8 @@
+package Collections.Map.SortedCollection;
+
+public class Main {
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bead" ,0.86, 100);
+
+    }
+}
